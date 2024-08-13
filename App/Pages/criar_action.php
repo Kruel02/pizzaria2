a55e622d89e89348c15f65bec2afda24d5b8203f
MOISES ALVES BANHARA DE OLIVEIRA <moises.aboliveira@senacsp.edu.br>
2024-08-12 21:58:56 -0300

    tentando filtrar os numeros quebrados

--- App/Pages/criar_action.php
+++ App/Pages/criar_action.php
@@ -1,9 +1,38 @@
 <?php
+    require_once("./../Config/config.php");
 
+ 
+ $_POST['ValorPizza'] = str_replace(',','.', $_POST['ValorPizza']);
+ var_dump($_POST['ValorPizza']); 
  $NomePizza = filter_input(INPUT_POST,'NomePizza', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
- $ValorPizza = filter_input(INPUT_POST,'ValorPizza',FILTER_VALIDATE_FLOAT);
+ $ValorPizza = filter_input(INPUT_POST, 'ValorPizza',FILTER_VALIDATE_FLOAT);
+ var_dump($ValorPizza);
  $TamanhoPizza = filter_input(INPUT_POST, 'TamanhoPizza', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
  $DescricaoPizza = $_POST['DescricaoPizza'];
+ 
 
- echo "Dados enviados {$NomePizza}, {$ValorPizza}, {$TamanhoPizza}, e {$DescricaoPizza}";
-var_dump($_POST);
+  if($NomePizza && $ValorPizza && $TamanhoPizza)
+  {
+     $sql = $pdo->prepare("INSERT INTO pizzas (IdUsuario, NomePizza, PathFoto, Valor, Tamanho, Descrição) VALUES 
+     (:IdUsuario,:NomePizza,:PathFoto,:Valor,:TamanhoPizza,:DescricaoPizza);") ;
+   var_dump($sql);
+   var_dump($pdo);
+    $sql->bindValue(":IdUsuario", 1);
+    $sql->bindValue(":NomePizza", $NomePizza) ;
+    $sql->bindValue(":TamanhoPizza", $TamanhoPizza) ;
+    $sql->bindValue(":DescricaoPizza", $DescricaoPizza) ;
+    $sql->bindValue(":Valor", $ValorPizza) ;
+    $sql->bindValue(":PathFoto", "https://www.pizzaprime.com.br/wp-content/uploads/2019/10/massa-de-pizza.jpeg") ;
+    $sql->execute();
+    header("Location: ./../index.php");
+  
+    exit();
+    
+  }
+  else{
+   echo"Não foi";
+
+
+  }
+  
+
