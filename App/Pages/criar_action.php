ee14106f68686c731850aa7f51413d9b9e701515
MOISES ALVES BANHARA DE OLIVEIRA <moises.aboliveira@senacsp.edu.br>
2024-08-14 22:16:08 -0300

    novas pastas e arrumado o desafio

--- App/Pages/criar_action.php
+++ App/Pages/criar_action.php
@@ -6,20 +6,31 @@ use Money\Money;
     require_once("./../Config/config.php");
 
   
- setlocale(LC_MONETARY,"pt-BR");
+ //setlocale(LC_MONETARY,"pt-BR");
+ $_POST['ValorPizza'] = floatval(str_replace(',','.', $_POST['ValorPizza']));
+ var_dump($_POST['ValorPizza']);
+ $_POST['ValorPizza'] = floatval(number_format($_POST['ValorPizza'],3,'.', ','));
+ var_dump($_POST['ValorPizza']);
+ $ValorPizza =  floatval($_POST['ValorPizza']);
+ var_dump($ValorPizza);
+ $ValorPizza = floatval(str_replace('.','' , $ValorPizza ));
+ //$ValorPizza = filter_input(INPUT_POST, 'ValorPizza',FILTER_VALIDATE_FLOAT);
+ var_dump($ValorPizza);
+
+
  $NomePizza = filter_input(INPUT_POST,'NomePizza', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
- $ValorPizza = filter_input(INPUT_POST, 'ValorPizza',FILTER_VALIDATE_FLOAT);
- //var_dump($ValorPizza);
- $TamanhoPizza = filter_input(INPUT_POST, 'TamanhoPizza', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $TamanhoPizza = filter_input(INPUT_POST, 'TamanhoPizza', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
  $DescricaoPizza = $_POST['DescricaoPizza'];
 
- $ValorPizza = number_format(str_replace([',', '.'], ['', '.'], $_POST['ValorPizza']), 2, '.', ',');
- var_dump($ValorPizza);
+ 
 
- $ValorPizza = number_format($ValorPizza, 2, ",", ".");
- var_dump($ValorPizza);
- $ValorPizza = floatval(str_replace(',', '.',  $ValorPizza));
- var_dump($ValorPizza);
+ //$ValorPizza = number_format(str_replace(',', '.',  $_POST['ValorPizza']), 3, '','');
+ 
+
+ 
+//  $ValorPizza = floatval($ValorPizza);
+//  var_dump($ValorPizza);
+ 
 
 // $Formatter = new NumberFormatter('pt-BR', NumberFormatter::CURRENCY);
  
--- App/index.php
+++ App/index.php
@@ -1,7 +1,17 @@
 <?php
 
 require_once "./config/config.php";
+require "./../App/Pages/Partials/Header.php"
 
-echo "Hello world";
+?>
 
-var_dump($pdo);
+HTML code here.
+
+
+
+<?php
+require "./../App/Pages/Partials/Carrinho.php"
+
+require "./../App/Pages/Partials/Footer.php"
+require "./../App/Pages/Partials/Modal.php"
+?>
